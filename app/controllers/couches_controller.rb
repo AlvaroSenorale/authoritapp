0b89127ec1285668c5d9ad10de5266e44344d55a
Alvaro Señorale <alvarosenorale@gmail.com>
2024-08-20 12:59:35 -0300

    add endpoint

--- app/controllers/couches_controller.rb
+++ app/controllers/couches_controller.rb
@@ -2,7 +2,21 @@ class CouchesController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.json { render json: Couch.all }
+      format.json { render json: couches_collection }
     end
+  end
+
+  def show
+    # debugger
+    respond_to do |format|
+      format.html
+      format.json { render json: Couch.find(params[:id]) }
+    end
+  end
+
+  private
+
+  def couches_collection
+    Couch.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "couches", to: "couches#index"
+  get "couches/:id", to: "couches#show"
 end
